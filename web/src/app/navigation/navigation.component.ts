88681318fce89f775c327f715b312e9844f52306
Kneelawk <kneelawk@gmail.com>
2021-03-06 14:20:45 -0800

    Improve nav shading

--- web/src/app/navigation/navigation.component.ts
+++ web/src/app/navigation/navigation.component.ts
@@ -1,14 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
 import { IndexService } from "../index.service";
-import { AlbumJson, ResponseResult } from "../index.types";
+import { AlbumJson } from "../index.types";
 
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
-  styleUrls: ['./navigation.component.css']
+  styleUrls: ['./navigation.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class NavigationComponent implements OnInit {
 
